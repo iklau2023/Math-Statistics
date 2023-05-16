0192b9c21ebb96802d05d868c9f3177cd89e66c3
Isaac Klausmeyer <118828742+iklau2023@users.noreply.github.com>
2023-05-16 07:45:36 -0500

    Update quadratic_equation.py

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -1,9 +1,7 @@
 import math
-import time
 a= float(input("a="))
 b= float(input("b="))
 c= float(input("c="))
 x1= ((b*-1) + math.sqrt(b**2 - 4*a*c))/2
 x2= ((b*-1) - math.sqrt(b**2 - 4*a*c))/2
 print(f'x= {x1} or {x2}')
-time.sleep(100)
